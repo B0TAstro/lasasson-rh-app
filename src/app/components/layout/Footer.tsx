fdb3adda5e546422063c73b27a114b192b4b1e5f
Tom Boullay <148534949+B0TAstro@users.noreply.github.com>
2025-07-15 11:08:03 +0200

    Update Footer.tsx

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,10 +1,26 @@
 // app/components/layout/Footer.tsx
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="">
-      Footer
+    <footer className="text-black py-6 pt-25 pb-25 px-5 md:px-10 lg:px-16">
+      <div className="container mx-auto px-4 text-center">
+        <p className="text-sm">
+          © {currentYear} La Sasson. Tous droits réservés.
+        </p>
+        <p className="text-xs text-gray-300 mt-3">
+          Made by{" "}
+          <a
+            href="https://github.com/B0TAstro"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[var(--color-primary)] hover:underline transition-colors"
+          >
+            Astro
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
